2644d6b50cfc594d82012a2f151f31a35475279b
johnnyho <johnnyhohohohohoho@gmail.com>
2025-10-04 12:38:41 +0800

    streaming

--- src/main/java/com/example/grpc/service/StockTradingServiceImpl.java
+++ src/main/java/com/example/grpc/service/StockTradingServiceImpl.java
@@ -6,7 +6,11 @@ import com.example.StockTradingServiceGrpc;
 import com.example.grpc.entity.Stock;
 import com.example.grpc.repository.StockRepository;
 import io.grpc.stub.StreamObserver;
-import org.springframework.grpc.server.service.GrpcService;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import java.time.Instant;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @GrpcService
 public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradingServiceImplBase {
@@ -27,5 +31,27 @@ public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradin
                 .build();
         responseObserver.onNext(stockResponse);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void subscribeStockPrice(StockRequest request, StreamObserver<StockResponse> responseObserver) {
+        String symbol=request.getStockSymbol();
+        try {
+            for (int i = 0; i < 10; i++) {
+                StockResponse stockResponse = StockResponse.newBuilder()
+                        .setStockSymbol(symbol)
+                        .setPrice(new Random().nextDouble(200))
+                        .setTimestamp(Instant.now().toString())
+                        .build();
+                System.out.println("Stock price update: " + stockResponse.getStockSymbol() +
+                        ", Price: " + stockResponse.getPrice() +
+                        ", Time: " + stockResponse.getTimestamp());
+                responseObserver.onNext(stockResponse);
+                TimeUnit.SECONDS.sleep(1);
+            }
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(e);
+        }
     }
 }
